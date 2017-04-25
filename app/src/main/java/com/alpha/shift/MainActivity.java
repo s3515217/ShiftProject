29a3c471b6bbdbfce610be28d15a13abaa5fc982
s3515217 <s3515217@rmit.edu.vn>
2017-04-26 08:51:48 +1000

    nothing yet

--- app/src/main/java/com/alpha/shift/MainActivity.java
+++ app/src/main/java/com/alpha/shift/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_new_shift);
+        setContentView(R.layout.activity_main);
 
     }
 }
