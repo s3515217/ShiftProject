4105500d886fe4b25c74e548645d102818ef8422
s3515217 <s3515217@rmit.edu.vn>
2017-05-23 22:01:25 +1000

    Calendar View

--- app/src/main/java/com/alpha/shift/AddShift.java
+++ app/src/main/java/com/alpha/shift/AddShift.java
@@ -12,43 +12,29 @@ import android.widget.TimePicker;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+import static java.lang.Integer.valueOf;
+
+public class AddShift extends AppCompatActivity {
 
     EditText startTimeText, dateText, endTimeText;
-    int hour_x, minute_x, date_x, month_x, year_x;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final Calendar cal = Calendar.getInstance();
-        date_x = cal.get(Calendar.DAY_OF_MONTH);
-        month_x = cal.get(Calendar.MONTH);
-        year_x = cal.get(Calendar.YEAR);
-        hour_x = cal.get(Calendar.HOUR_OF_DAY);
-        minute_x = cal.get(Calendar.MINUTE);
-
-        showDatePickerOnClick();
-        showTimePickerOnClick();
-    }
-
-    public void showDatePickerOnClick() {
-        dateText = (EditText) findViewById(R.id.TextDate);
-        dateText.setText(date_x+"/"+month_x+1+"/"+year_x);
-        dateText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDialog(3);
-            }
-        });
-    }
+    int hour_x, minute_x, day_x, month_x, year_x;
 
     public void showTimePickerOnClick() {
         startTimeText = (EditText) findViewById(R.id.TextStartTime);
         endTimeText = (EditText) findViewById(R.id.TextEndTime);
+        dateText = (EditText) findViewById(R.id.TextDate);
+
+        final Calendar cal = Calendar.getInstance();
+        hour_x = cal.get(Calendar.HOUR_OF_DAY);
+        minute_x = cal.get(Calendar.MINUTE);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+        month_x = cal.get(Calendar.MONTH);
+        year_x = cal.get(Calendar.YEAR);
+
         endTimeText.setText("00:00");
         startTimeText.setText("00:00");
+        dateText.setText(day_x + "/" + valueOf(month_x+1) + "/" + year_x);
+
         startTimeText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,29 +42,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         endTimeText.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 showDialog(2);
             }
         });
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(3);
+            }
+        });
+
+
     }
 
     @Override
     protected Dialog onCreateDialog(int id) {
-        if (id == 1) {
-            return new TimePickerDialog(this,timePickerListenerForStartTime,hour_x,minute_x,false);
-        }
-        if (id == 2) {
-            return new TimePickerDialog(this,timePickerListenerForEndTime,hour_x,minute_x,false);
-        }
-        if (id == 3) {
-            return new DatePickerDialog(this,datePickerListener,year_x,month_x,date_x);
-        }
+     if (id == 1) {
+         return new TimePickerDialog(this,startTimePickerListener,hour_x,minute_x,false);
+     }
+     if (id == 2) {
+         return new TimePickerDialog(this,endTimePickerListener,hour_x,minute_x,false);
+     }
+     if (id == 3) {
+         return new DatePickerDialog(this,datePickerListener,year_x,month_x ,day_x);
+     }
         return null;
     }
 
-    private TimePickerDialog.OnTimeSetListener timePickerListenerForStartTime
+    private TimePickerDialog.OnTimeSetListener startTimePickerListener
             = new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -88,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private TimePickerDialog.OnTimeSetListener timePickerListenerForEndTime
+    private TimePickerDialog.OnTimeSetListener endTimePickerListener
             = new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -102,10 +95,19 @@ public class MainActivity extends AppCompatActivity {
             = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-            date_x=dayOfMonth;
+            day_x=dayOfMonth;
             month_x=month+1;
             year_x=year;
-            dateText.setText(date_x + "/" + month_x + "/" + year_x);
+            dateText.setText(day_x + "/" + month_x + "/" + year_x);
         }
     };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_shift);
+
+        showTimePickerOnClick();
+    }
 }
